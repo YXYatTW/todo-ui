bc00d2c9f90cf9df2a92d9aeaef21d4ab636c7b3
kerryuan <xinyi.yuan@thoughtworks.com>
2025-12-19 13:50:23 +0800

    apollo client setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-"use client";
+'use client';
 
-import styled from "styled-components";
-import TaskBoard from "@/components/TaskBoard";
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+import styled from 'styled-components';
+import TaskBoard from '@/components/TaskBoard';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3002/graphql' }),
+  cache: new InMemoryCache(),
+});
 
 export default function Home() {
   return (
-    <Wrapper>
-      <TaskBoard />
-    </Wrapper>
+    <ApolloProvider client={client}>
+      <Wrapper>
+        <TaskBoard />
+      </Wrapper>
+    </ApolloProvider>
   );
 }
 
