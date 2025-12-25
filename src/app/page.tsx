958c8b14c97943ee53761abb4113de358a2bac57
kerryuan <xinyi.yuan@thoughtworks.com>
2025-12-25 13:55:44 +0800

    Update URI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
 
 import styled from 'styled-components';
 import TaskBoard from '@/components/TaskBoard';
 
+const graphqlUri =
+  process.env.NEXT_PUBLIC_GRAPHQL_URI || 'http://localhost:3002/graphql';
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:3002/graphql' }),
+  link: new HttpLink({
+    uri: graphqlUri,
+  }),
   cache: new InMemoryCache(),
 });
 
